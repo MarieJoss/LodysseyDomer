9b611b705773905a67b505d4d5618a8a4d570b4b
Marie Joss <mjosselin6@gmail.com>
2020-07-09 16:12:18 +0200

    material ui

--- front/lodysseydomer_front/src/components/SignUp.jsx
+++ front/lodysseydomer_front/src/components/SignUp.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
-import { Form, FormGroup } from "reactstrap";
+import { Form } from "reactstrap";
+
+import styles from "./signup.module.css";
+import Button from "@material-ui/core/Button";
 
 import Axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 function SignUp() {
   const [name, setPrenom] = useState("");
   const [lastname, setNom] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [verifPassword, setVerifPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,63 +39,70 @@ function SignUp() {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <h1>
-        {name}
-        <br />
-        {lastname}
-        <br />
-        {email}
-        <br />
-        {password}
-      </h1>
-      <FormGroup>
-        <input
-          name="prenom"
-          type="text"
-          value={name}
-          placeholder="Votre prénom"
-          onChange={(e) => setPrenom(e.target.value)}
-        />
-      </FormGroup>
-      <FormGroup>
-        <input
-          name="nom"
-          value={lastname}
-          type="text"
-          placeholder="Votre nom de famille"
-          onChange={(e) => setNom(e.target.value)}
-        />
-      </FormGroup>
-      <FormGroup>
-        <input
-          type="email"
-          name="email"
-          placeholder="Votre e-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormGroup>
-      <FormGroup>
-        <input
-          name="password"
-          value={password}
-          placeholder="Votre mot de passe"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormGroup>
-      <FormGroup>
-        <input
-          name="password"
-          value={verifPassword}
-          placeholder="Vérification mot de passe"
-          type="password"
-          onChange={(e) => setVerifPassword(e.target.value)}
-        />
-      </FormGroup>
-      <input type="submit" value="Envoyer" />
-    </Form>
+    <>
+      <h1>Inscription</h1>
+      <Form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <TextField
+              name="prenom"
+              type="text"
+              value={name}
+              onChange={(e) => setPrenom(e.target.value)}
+              id="outlined-basic"
+              label="Prénom"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              name="nom"
+              value={lastname}
+              type="text"
+              placeholder="Votre nom de famille"
+              onChange={(e) => setNom(e.target.value)}
+              id="outlined-basic"
+              label="Nom"
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <TextField
+              type="email"
+              name="email"
+              placeholder="Votre e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              name="password"
+              value={password}
+              placeholder="Votre mot de passe"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+        <Button
+          variant="contained"
+          size="medium"
+          type="submit"
+          className={styles.button}
+        >
+          S'INSCRIRE
+        </Button>
+      </Form>
+    </>
   );
 }
 
