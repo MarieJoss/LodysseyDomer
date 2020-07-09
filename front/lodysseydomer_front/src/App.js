2eb54fb2ca14b6a86a12804b190ddcc89f7c709f
Marie Joss <mjosselin6@gmail.com>
2020-07-09 17:25:22 +0200

    router

--- front/lodysseydomer_front/src/App.js
+++ front/lodysseydomer_front/src/App.js
@@ -1,38 +1,20 @@
 import React from "react";
 import "./App.css";
 import SignUp from "./components/SignUp";
+import router from "./router";
+// import SignIn from "./components/SignIn";
+// import Profile from "./components/Profile";
+
 import { MuiThemeProvider, Grid, Paper } from "@material-ui/core";
+import Router from "./router";
 
 function App() {
   return (
     <div className="App">
       <MuiThemeProvider>
-        <Grid
-          container
-          alignItems="center"
-          style={{
-            height: "100vh",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          <Grid item xs={5}>
-            <Paper
-              elevation={20}
-              style={{
-                borderRadius: 20,
-                padding: 30,
-                backgroundColor: "#F7F7F6",
-              }}
-            >
-              <Grid container alignItems="center" justify="center">
-                <Grid item xs={12} alignContent="center">
-                  <SignUp />
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-        </Grid>
+        <Router />
+        {/* <SignIn /> */}
+        {/* <Profile /> */}
       </MuiThemeProvider>
     </div>
   );
